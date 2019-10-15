ff3cc7845a1a95b07cdce722af176b6d4905bd7d
Dylan Sprague <dsprague@docuphase.com>
2019-10-15 12:54:27 -0400

    Correct names

--- AuthSystem-password-service/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AuthSystem_password_service
-{
-    public class Class1
-    {
-    }
-}
---
+++ AuthSystemPasswordService/ValueTypes.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AuthSystemPasswordService
+{
+    public class ValueTypes
+    {
+    }
+}
