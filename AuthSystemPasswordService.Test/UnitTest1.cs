276061f661c9e51a9559d63d61f812b69817c608
Dylan Sprague <dsprague@docuphase.com>
2019-10-15 13:03:33 -0400

    Create Test project

---
+++ AuthSystemPasswordService.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AuthSystemPasswordService.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
