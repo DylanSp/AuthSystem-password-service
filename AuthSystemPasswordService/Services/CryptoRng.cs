c640c2ecff9f552258d344b83a5c4bc61bd9f590
Dylan Sprague <dsprague@docuphase.com>
2019-10-15 13:02:43 -0400

    Add services

---
+++ AuthSystemPasswordService/Services/CryptoRng.cs
@@ -0,0 +1,22 @@
+﻿using AuthSystemPasswordService.Interfaces;
+using System.Security.Cryptography;
+
+namespace AuthSystemPasswordService.Services
+{
+    public class CryptoRng : ICryptoRng
+    {
+        private RandomNumberGenerator Generator { get; }
+
+        public CryptoRng()
+        {
+            Generator = RandomNumberGenerator.Create();
+        }
+
+        public byte[] GetRandomBytes(int numBytes)
+        {
+            var bytes = new byte[numBytes];
+            Generator.GetBytes(bytes);
+            return bytes;
+        }
+    }
+}
---
+++ AuthSystemPasswordService/Services/PasswordService.cs
@@ -0,0 +1,38 @@
+﻿using AuthSystemPasswordService.Interfaces;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Linq;
+
+namespace AuthSystemPasswordService.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        private KeyDerivationParameters Parameters { get; }
+        private ICryptoRng Rng { get; }
+
+        public PasswordService(KeyDerivationParameters parameters, ICryptoRng rng)
+        {
+            Parameters = parameters;
+            Rng = rng;
+        }
+
+        public HashedPassword GeneratePasswordHashAndSalt(PlaintextPassword password)
+        {
+            var saltBytes = Rng.GetRandomBytes(Parameters.SaltLength.Value);
+            var salt = Convert.ToBase64String(saltBytes);
+
+            var hashBytes = KeyDerivation.Pbkdf2(password.Value, saltBytes, Parameters.DerivationFunction,
+                Parameters.IterationCount.Value, Parameters.KeyLength.Value);
+            var hash = Convert.ToBase64String(hashBytes);
+
+            return new HashedPassword(Base64Hash.From(hash), Base64Salt.From(salt));
+        }
+
+        public bool CheckIfPasswordMatchesHash(PlaintextPassword password, HashedPassword hash)
+        {
+            var passwordHash = KeyDerivation.Pbkdf2(password.Value, Convert.FromBase64String(hash.Base64Salt.Value), Parameters.DerivationFunction,
+                Parameters.IterationCount.Value, Parameters.KeyLength.Value);
+            return passwordHash.SequenceEqual(Convert.FromBase64String(hash.Base64PasswordHash.Value));
+        }
+    }
+}
